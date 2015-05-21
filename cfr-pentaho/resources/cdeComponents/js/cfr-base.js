d85984ce59527fcb1aedbde208bf4eceec62b49e
David Santos <david.santos@webdetails.pt>
2015-05-21 15:54:25 +0100

    [CFR-3432] Fixed FileBrowserComponent to work with Ctools 13.09.10

---
+++ cfr-pentaho/resources/cdeComponents/js/cfr-base.js
@@ -0,0 +1,18 @@
+var Endpoints = {
+
+  //The webAppPath is defined at the start of Dashboards.js
+  getWebappBasePath: function () {
+    return webAppPath;
+  },
+
+  getFile: function() {
+    return this.getWebappBasePath() + "/content/cfr/getFile";
+  },
+
+  getListFiles: function () {
+    return this.getWebappBasePath() + "/content/cfr/listFiles";
+  },
+  getStore: function() {
+    return this.getWebappBasePath() + "/content/cfr/store";
+  }
+};
